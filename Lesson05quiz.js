bd0189b25829e4bd94d7d16150cab0dbf1c78042
18016165 <18016165@myrp.edu.sg>
2019-11-18 15:42:55 +0800

    Quiz

--- Lesson05quiz.js
+++ Lesson05quiz.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import {View} from 'react-native';
-//INSERT DATA HERE
+import {View, Text, Image, ScrollView} from 'react-native';
+
+const PET_DATA =
+    {
+        name: 'Gizmo',
+        age: '5',
+        image: require('./src/img/bird.jpg')
+    };
+
+const OWNER_DATA =
+    {
+        name: 'Daniyah Bai',
+        contact: '97502219',
+        membership: 'gold'
+    };
 
 const Lesson05quiz = () => {
-  return (
-    <View>
-
-    </View>
-  );
+    
+    return (
+        <ScrollView>
+            <View>
+                <Text>Name:</Text>
+                <Text>----------{PET_DATA.name}</Text>
+    
+                <Text>Age:</Text>
+                <Text>----------{PET_DATA.age} years old</Text>
+    
+                <Image source = {PET_DATA.image} />
+                
+                <Text>Owner Information</Text>
+                <Text>---------------</Text>
+    
+                <Text>Name:</Text>
+                <Text>----------{OWNER_DATA.name}</Text>
+    
+                <Text>Contact:</Text>
+                <Text>----------{OWNER_DATA.contact}</Text>
+    
+                <Text>Membership:</Text>
+                <Text>----------{OWNER_DATA.membership}</Text>
+            </View>
+        </ScrollView>
+    );
 };
 export default Lesson05quiz;
